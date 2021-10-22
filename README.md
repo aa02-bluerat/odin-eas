f65f5e1d84c1af462ec5d3f7331e5b38f9f127bc
aa02-bluerat <blueratdota@gmail.com>
2021-10-22 17:49:54 +0800

    v1.1

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # odin-eas
 # v1.0 control box doesn't function yet
+# v1.1 +clear=erase all bg
+#      +colormode=black brush
+#      +eraser=brush->bg color
--- script.js
+++ script.js
@@ -1,15 +1,31 @@
+//drawing stuff
 const contSketch = document.querySelector('.sketch');
-const grabGrid = document.querySelectorAll('.grid');
+
+
+//buttons
+const btnColorMode = document.querySelector('#color-mode')
+const btnRGBMode = document.querySelector('#rgb-mode')
+const btnUpdateBG = document.querySelector('#set-background')
+const btnEraserMode = document.querySelector('#eraser-mode')
+const btnClear = document.querySelector('#nuke-mode')
+const btnUpdateSize = document.querySelector('#sizing-mode')
 
 // let gridSize = Number(prompt('grid size?'))
-let gridSize = Number(32);
+let gridSize = Number(40);
+
+//defaults
 let keyStatus = '';
+let bgDefault = 'white';
+let cBackground = 'white';
+let brushDefault = 'black';
+let cBrush='black';
 
-
-window.addEventListener('mousedown', function(){
+window.addEventListener('mousedown', function(e){
     console.log('key pressed');
     keyStatus = 'pressed';
     console.log(keyStatus);
+    e.preventDefault(); 
+    // god damn, what a fix::once no longer mousedown, cancel all related to this event
 })
 window.addEventListener('mouseup', function(){
     console.log('key lifted');
@@ -18,20 +34,35 @@ window.addEventListener('mouseup', function(){
 })
 window.onload = ()=>{
     const grabGrid = document.querySelectorAll('.grid');
+    // console.log(grabGrid);
     grabGrid.forEach((box) => {box.addEventListener('mouseover', function(){
-        if (keyStatus=='pressed'){box.style.backgroundColor='black';}   
+        if (keyStatus=='pressed'){box.style.backgroundColor=`${cBrush}`;}   
     })})   
 }
 
 
-
-
-
-
 for (let i =0; i<gridSize*gridSize; i++){
     const makeDiv = document.createElement('div');
-    makeDiv.textContent=''
-    makeDiv.classList.add('grid')
-    makeDiv.style.flex = `1 0 ${100/gridSize}%`
-    contSketch.appendChild(makeDiv)
+    makeDiv.textContent='';
+    makeDiv.classList.add('grid');
+    makeDiv.style.flex = `1 0 ${100/gridSize}%`;
+    contSketch.appendChild(makeDiv);
 }
+
+btnClear.addEventListener('click', function(){
+    const grabGrid = document.querySelectorAll('.grid');
+    console.log('clear');
+    grabGrid.forEach((box) => {box.style.backgroundColor=`${bgDefault}`;})
+})
+
+btnEraserMode.addEventListener('click',function(){
+    cBrush=cBackground;
+})
+btnColorMode.addEventListener('click',function(){
+    cBrush=brushDefault;
+})
+
+
+//add color picker
+//add background picker
+//add grid size selector
